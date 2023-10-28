90de73300c1949d225c5dcd39c0e50e5b9009588
Jeremy Price <jrp114@yahoo.com>
2023-10-28 07:47:32 -0400

    added toggle button

---
+++ src/components/toggle-button.tsx
@@ -0,0 +1,23 @@
+import classNames from 'classnames';
+
+interface ToggleButtonProps {
+  toggle: boolean;
+  handleToggle: () => void;
+}
+
+export default function ToggleButton(props: ToggleButtonProps) {
+  return (
+    <div
+      className={classNames(
+        'w-10 h-6 flex items-center border bg-blue-700 rounded-xl relative cursor-pointer',
+        {
+          'justify-start': !props.toggle,
+          'justify-end': props.toggle,
+        },
+      )}
+      onClick={props.handleToggle}
+    >
+      <div className="w-5 h-5 bg-white rounded-full absolute"></div>
+    </div>
+  );
+}
