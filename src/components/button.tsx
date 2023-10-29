b78e3ef4650dd7c4f3fe2cc8890496873ab20078
Jeremy Price <jrp114@yahoo.com>
2023-10-29 08:57:25 -0400

    updated button with size prop

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,20 +4,24 @@ interface ButtonProps {
   label: string;
   onClick?: () => void;
   variant?: 'primary' | 'secondary';
+  size?: 'sm' | 'md' | 'lg';
 }
 
 export default function Button(props: ButtonProps) {
-  const { variant = 'primary' } = props;
+  const { variant = 'primary', size = 'md' } = props;
   return (
     <button
       type="button"
       onClick={props.onClick}
       className={classNames(
-        'text-xs font-bold border-2 border-gray-300 rounded-md p-2 px-10 min-w-[200px]',
+        'text-xs font-bold border-2 border-gray-300 rounded-md',
         {
           'bg-primary text-white': variant === 'primary',
           'bg-white border border-secondary text-secondary':
             variant === 'secondary',
+          'p-1 min-w-[175px] px-8': size === 'sm',
+          'p-2 min-w-[200px] px-10': size === 'md',
+          'p-4': size === 'lg',
         },
       )}
     >
