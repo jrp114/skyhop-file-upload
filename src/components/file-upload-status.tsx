ab88d6d9f76c9fb74f6901ff12e82ecf1194e382
Jeremy Price <jrp114@yahoo.com>
2023-10-29 07:20:50 -0400

    added file upload status

---
+++ src/components/file-upload-status.tsx
@@ -0,0 +1,47 @@
+interface FileUploadStatusProps {
+  current: number;
+  width: number;
+  height: number;
+  scale: number;
+}
+
+export default function FileUploadStatus(props: FileUploadStatusProps) {
+  return (
+    <>
+      <div className="border-t border-gray-300 my-3" />
+      <div className="pl-2 flex gap-3 items-center">
+        <img className="h-5 w-5" src="./file-image.svg" alt="file" />
+        <div>
+          <div className="flex flex-row justify-between w-full">
+            <div className="text-gray-300 text-sm">File Name</div>
+            <div className="text-gray-500 text-xs">5.7MB</div>
+          </div>
+          <svg
+            height={props.height}
+            width={props.width}
+            viewBox={`0 0 ${props.width} ${props.height}`}
+            preserveAspectRatio="xMinYMin meet"
+          >
+            <g>
+              <rect
+                x={0}
+                y={0}
+                width={props.width}
+                height={props.height}
+                className="fill-gray-300"
+              />
+              <rect
+                x={0}
+                y={0}
+                width={props.current * props.scale}
+                height={props.height}
+                className="fill-secondary"
+              />
+            </g>
+          </svg>
+        </div>
+      </div>
+      <div className="border-t border-gray-300 my-3" />
+    </>
+  );
+}
