bb4f6ea612b8d31d51dbd171639f7f141a695fbe
Jeremy Price <jrp114@yahoo.com>
2023-10-30 08:11:48 -0400

    slight tweek to toggle

--- src/components/toggle-button.tsx
+++ src/components/toggle-button.tsx
@@ -9,7 +9,7 @@ export default function ToggleButton(props: ToggleButtonProps) {
   return (
     <div
       className={classNames(
-        'w-10 h-6 flex items-center border bg-primary rounded-xl relative cursor-pointer',
+        'w-9 h-5 flex items-center border bg-primary rounded-xl relative cursor-pointer',
         {
           'justify-start': !props.toggle,
           'justify-end': props.toggle,
@@ -17,7 +17,7 @@ export default function ToggleButton(props: ToggleButtonProps) {
       )}
       onClick={props.handleToggle}
     >
-      <div className="w-5 h-5 bg-white rounded-full absolute"></div>
+      <div className="w-4 h-4 bg-white rounded-full absolute"></div>
     </div>
   );
 }
